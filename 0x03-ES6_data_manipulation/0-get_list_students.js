9438a738d0dc83fa82668f4147602197212c3ee9
QueJobe <63298281+QueJobe@users.noreply.github.com>
2024-08-13 01:31:25 +0200

    Update 0-get_list_students.js
    upgrade

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
-export default function getListStudents(){
-    return[
+const getListStudents = () => [
 		{id: 1, firstname: 'Guillaume', location: 'San Francisco' },
 		{id: 2, firstname: 'James', location: 'Columbia'},
 		{id: 5, firstname: 'Serena', location: 'San Francisco'}
 	];
-}
+
+export default getListStudents;
